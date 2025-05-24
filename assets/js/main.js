12eeba9e619b9c5aebfe33681db69fe97d5f460f
André Carvalho <avgcf2003@gmail.com>
2025-05-23 21:03:09 -0300

    a

---
+++ assets/js/main.js
@@ -0,0 +1,105 @@
+import { desafios } from "./desafios.js";
+
+const navigation = document.querySelector("#navigation");
+const backToTopButton = document.querySelector("#backToTopButton");
+const toggle = document.querySelector("#sw-checkbox");
+
+window.addEventListener("scroll", onScroll);
+onScroll();
+
+window.onload = setTimeout(() => {
+  notebook_1.style.opacity = 0;
+
+  notebook_1.style.animation = "none";
+  notebook_2.style.animation = "none";
+  notebook_2_white.style.animation = "none";
+  vidro.style.animation = "none";
+}, 4000);
+
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(knowledge);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+openMenu();
+function openMenu() {
+  const openBtns = document.querySelectorAll(".open");
+  openBtns.forEach((e) => {
+    e.addEventListener("click", () => {
+      document.body.classList.add("menu-expanded");
+    });
+  });
+}
+
+closeMenu();
+function closeMenu() {
+  const closeBtns = document.querySelectorAll(".close");
+  closeBtns.forEach((e) => {
+    e.addEventListener("click", () => {
+      document.body.classList.remove("menu-expanded");
+    });
+  });
+}
+
+ScrollReveal({
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
+}).reveal(
+  `#home, 
+  #home img, 
+  #about, 
+  #about header, 
+  #about p,
+  #about img,
+  #projects,
+  #projects header,
+  #projects .card,
+  #knowledge,
+  #knowledg header,
+  #knowledg .card,
+  #contact,
+  #contact header`
+);
